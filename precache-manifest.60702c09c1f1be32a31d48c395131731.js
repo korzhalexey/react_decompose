d30762cb0bf098b3a5b09a598b7d1d3f8e5119d8
korzh <korzh.alexey@yahoo.com>
2021-07-13 18:42:05 +0300

    Updates

---
+++ precache-manifest.60702c09c1f1be32a31d48c395131731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41f92be986cd354077be4387d4a6d10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0b8d139189f8642cfb3",
+    "url": "./static/css/main.75a36f0f.chunk.css"
+  },
+  {
+    "revision": "b1bcebc34025309b4591",
+    "url": "./static/js/2.6b5bd82a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6b5bd82a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b8d139189f8642cfb3",
+    "url": "./static/js/main.ea538de5.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
